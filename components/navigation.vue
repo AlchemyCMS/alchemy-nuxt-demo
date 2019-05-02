abb023d04ab5fd13ffdc853619a23e45d159266a
Thomas von Deyen <thomas@vondeyen.com>
2019-05-02 20:59:45 +0200

    Add a simple static page navigation

---
+++ components/navigation.vue
@@ -0,0 +1,14 @@
+<template>
+  <b-nav pills>
+    <b-nav-item>
+      <n-link to="/" class="nav-link" active-class="active" exact>
+        Startseite
+      </n-link>
+    </b-nav-item>
+    <b-nav-item>
+      <n-link to="/about-us" class="nav-link" active-class="active" exact>
+        About us
+      </n-link>
+    </b-nav-item>
+  </b-nav>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <navigation />
     <nuxt />
   </div>
 </template>
+
+<script>
+import Navigation from '~/components/navigation'
+
+export default {
+  components: {
+    Navigation
+  }
+}
+</script>
