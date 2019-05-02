68b342d38123375b88d0e61c3a20d54e3be96a22
Thomas von Deyen <thomas@vondeyen.com>
2019-05-02 18:02:06 +0200

    Install nuxt/apollo module

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,13 +39,56 @@ export default {
     '@nuxtjs/axios',
     // Doc: https://bootstrap-vue.js.org/docs/
     'bootstrap-vue/nuxt',
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
+    '@nuxtjs/apollo'
   ],
   /*
    ** Axios module configuration
    */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
+  },
+  server: {
+    port: 4000
+  },
+  apollo: {
+    // tokenName: 'yourApolloTokenName', // optional, default: apollo-token
+    // tokenExpires: 10, // optional, default: 7 (days)
+    // includeNodeModules: true, // optional, default: false (this includes graphql-tag for node_modules folder)
+    // authenticationType: 'Basic', // optional, default: 'Bearer'
+    // optional
+    // errorHandler(error) {
+    //   console.log('%cError', 'background: red; color: white; padding: 2px 4px; border-radius: 3px; font-weight: bold;', error.message)
+    // },
+    // required
+    clientConfigs: {
+      default: {
+        // required
+        httpEndpoint: 'http://localhost:3000/graphql',
+        // optional
+        // See https://www.apollographql.com/docs/link/links/http.html#options
+        // httpLinkOptions: {
+        //   credentials: 'same-origin'
+        // },
+        // You can use `wss` for secure connection (recommended in production)
+        // Use `null` to disable subscriptions
+        // wsEndpoint: 'ws://localhost:4000', // optional
+        // LocalStorage token
+        // tokenName: 'apollo-token', // optional
+        // Enable Automatic Query persisting with Apollo Engine
+        // persisting: false, // Optional
+        // Use websockets for everything (no HTTP)
+        // You need to pass a `wsEndpoint` for this to work
+        // websocketsOnly: false // Optional
+      },
+      // test: {
+      //   httpEndpoint: 'http://localhost:3000',
+      //   wsEndpoint: 'ws://localhost:3000',
+      //   tokenName: 'apollo-token'
+      // },
+      // alternative: user path to config which returns exact same config options
+      // test2: '~/plugins/my-alternative-apollo-config.js'
+    }
   },
 
   /*
