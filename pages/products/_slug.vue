76aaccf018bbaf9e02dd03788f53a304e2c05d70
Thomas von Deyen <thomas@vondeyen.com>
2019-05-02 21:00:39 +0200

    Add a dynamic products route
    This fetches alchemy pages by url (product slug)

---
+++ pages/products/_slug.vue
@@ -0,0 +1,18 @@
+<template>
+  <alchemy-page :page-url="pageUrl" />
+</template>
+
+<script>
+import AlchemyPage from '~/components/alchemy-page'
+
+export default {
+  components: {
+    AlchemyPage
+  },
+  computed: {
+    pageUrl() {
+      return `products/${this.$route.params.slug}`
+    }
+  }
+}
+</script>
